3ec0d63f48e3b2548c5ecf8a370ebfa272b92c13
rnpsmun2022 <rnpsmun2022@outlook.com>
2023-01-29 01:44:29 +0530

    UserChain Validation Hashing Bug not Fixed

--- BlockChain/UserChain.js
+++ BlockChain/UserChain.js
@@ -5,8 +5,27 @@ class UserChain extends BlockChain {
     constructor() { super() }
 
     addUser(user) {
-        const block = new Block(this.chain.length, new Date, user)
+        let hashedSaltedUser = user
+        hashedSaltedUser.password = SHA256(hashedSaltedUser.password)
+        const block = new Block(this.chain.length, new Date, hashedSaltedUser)
         this.addBlock(block)
+    }
+
+    findUser(username){
+        for(let i=this.chain.length-1; i>0; i--) {
+            if(this.chain[i].content.username == username) { 
+                return this.chain[i].content;
+            }
+        }
+        return null;
+    }
+
+    verifyUser(username,password){
+        let user = this.findUser(username)
+        let hashedSaltedPassword = SHA256(password)
+        if(hashedSaltedPassword == user.password) {
+            return true
+        } else return false 
     }
 
     transaction(from, to, amt) {
@@ -30,13 +49,14 @@ class UserChain extends BlockChain {
             newtoBlock.deposit = newtoBlock.deposit + amt
             this.addUser(newfromBlock)
             this.addUser(newtoBlock)
-        }
+        } else return "Not enough deposit"
     }
 
 }
 
 let myBlockChain = new UserChain
-myBlockChain.addUser({'id':1, 'name':'Anirudh', 'deposit':20})
-myBlockChain.addUser({'id':2, 'name':"Laaksh", 'deposit':10})
+myBlockChain.addUser({'id':1, 'username':'Anirudh', 'password':'qwerty', 'deposit':20})
+myBlockChain.addUser({'id':2, 'username':"Laaksh", 'password': '123456','deposit':10})
 myBlockChain.transaction(1,2,10)
 console.log(myBlockChain.chain)
+console.log(myBlockChain.verifyUser('Anirudh','qwerty'))

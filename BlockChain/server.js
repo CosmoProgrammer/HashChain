b635b6ab210d1d05ede6c87172835f2c03fe5c68
lmc212006 <laaksh.chikodi@gmail.com>
2023-01-30 21:20:43 +0530

    Did some stuff

--- BlockChain/server.js
+++ BlockChain/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express(); 
-const port = 5000;
+const port = 7863
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
@@ -8,8 +8,21 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
     res.send('The Blockchain serverside is working')
+    if (true){
+        console.log('test')
+    }
 })
 
+app.post('/', (req, res) => {
+    var username = JSON.parse(req.body.username)
+    var pswd = JSON.parse(req.body.password)
+    var item = JSON.parse(req.body.item)
+    var func = JSON.parse(req.body.func)
+})
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
+

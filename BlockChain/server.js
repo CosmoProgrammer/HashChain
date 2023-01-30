2e27e64366baab89b2a9813e374a0cc42c0ec663
lmc212006 <laaksh.chikodi@gmail.com>
2023-01-30 21:33:05 +0530

    Update server.js

--- BlockChain/server.js
+++ BlockChain/server.js
@@ -8,21 +8,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
+    var username = JSON.parse(req.body.username)
+    var pswd = JSON.parse(req.body.password)
+    var item = JSON.parse(req.body.item)
+    var func = JSON.parse(req.body.func)
     res.send('The Blockchain serverside is working')
     if (true){
         console.log('test')
     }
 })
 
-app.post('/', (req, res) => {
-    var username = JSON.parse(req.body.username)
-    var pswd = JSON.parse(req.body.password)
-    var item = JSON.parse(req.body.item)
-    var func = JSON.parse(req.body.func)
+app.get('/sendItem/:item', (req, res) =>{
+    var item = 
 })
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
 

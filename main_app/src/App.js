43273f21f36a6ece8d8826b0c1e61ac5cc5004a0
rnpsmun2022 <rnpsmun2022@outlook.com>
2023-01-30 23:35:18 +0530

    Rage quit tree

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -2,12 +2,45 @@ import './App.css';
 import "react-toastify/dist/ReactToastify.css";
 import { Routes as Switch, Route } from 'react-router-dom';
 import Login from './containers/login';
+import Tree from './containers/Tree';
 
 function App() {
+    const item = {
+        location: "Peril Farm",
+        name: "Rice grains",
+        description: "Rice grains",
+        quantity: "500",
+        expirationDate: "12/3/23",
+        sourceInfo: "Farm",
+        cost: 1000,
+        compliance: {
+          temperature: (temp) => temp <= 20,
+          moisture: (moisture) => moisture <= 60,
+        },
+        componentItems: [
+            {
+                location: "Peril Farm",
+                name: "Rice grains",
+                description: "Rice grains",
+                quantity: "500",
+                expirationDate: "12/3/23",
+                sourceInfo: "Farm",
+                cost: 1000,
+                compliance: {
+                  temperature: (temp) => temp <= 20,
+                  moisture: (moisture) => moisture <= 60,
+                },
+                componentItems: [
+                  
+                ]
+            }
+        ]
+    };
     return (
       <>
       <Switch>
         <Route path="/" exact element={ <Login /> } />
+        <Route path="/tree" exact element={ <Tree item={item}/> } />
       </Switch>
       </>
     );

1761cbded9ba2cbcb1f38b1f5314640e5cf33c53
MrSkywalker123 <73058245+MrSkywalker123@users.noreply.github.com>
2023-01-31 12:28:05 +0530

    Made some changes

--- main_app/src/containers/login.js
+++ main_app/src/containers/login.js
@@ -13,17 +13,15 @@ function Login(){
       if(request.readyState===4 && request.status ===200){
         if(this.responseText==='false' || this.responseText === false){
           localStorage.setItem('authenticated',this.responseText);
-          return false;
         }
         else{
           localStorage.setItem("authenticated",true);
           let Var = JSON.parse(this.responseText);
           localStorage.setItem('username', Var['entered_username']);
           localStorage.setItem('password', Var['entered_password']);
-          return true;
         }
       }
-        request.open('GET','http://localhost:7863'+JSON.stringify(credentials),true);
+        request.open('GET','http://localhost:7863/login/'+JSON.stringify(credentials),true);
         request.send();
     }
 

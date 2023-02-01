1d65bffee1be68c126179b4f12585fb10c9be677
Parth <pkd560010@gmail.com>
2023-02-02 00:42:34 +0530

    home page css

--- main_app/src/containers/Home.js
+++ main_app/src/containers/Home.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/home.css";
+import webcon from '../styles/ICON.png'
 
 const HomePage = () => {
   return (
+    <div className="main">
     <div className="home-page">
       <div className="header">
-        <h1 className="title">Supply Chain Management System</h1>
+        <li>
+        <ul className="title"><img src={webcon} style={{height:'110px'}}/>Supply Chain Management System</ul>
+        <ul className="subhead">"A mathematical framework that is free of politics and human error"</ul></li>
+        <div className="navbar">
+          <ul>
+            <li><a><Link to="/qr">
+              About Us
+            </Link></a></li>
+            <li><a><Link to="/search">
+              Contact Us
+            </Link></a></li>
+            <li><a><Link to="/search">
+              About Project
+            </Link></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="actions">
+          <Link to="/qr" className="scan-button">
+            Scan QR Code
+          </Link>
+          <Link to="/search" className="search-button">
+            Search by ID
+          </Link>
+      </div>
+      <div>
         <p className="description">
           Powered by blockchain technology for end-to-end transparency and
           security
         </p>
       </div>
-      <div className="actions">
-        <Link to="/qr" className="scan-button">
-          Scan QR Code
-        </Link>
-        <Link to="/search" className="search-button">
-          Search by ID
-        </Link>
-      </div>
+    </div>
     </div>
   );
 };

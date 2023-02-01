d9a9720b3ccf19298f8731807948d51eb3de5d99
Parth <pkd560010@gmail.com>
2023-02-01 12:09:57 +0530

    ye

--- main_app/src/containers/NoPageFound.js
+++ main_app/src/containers/NoPageFound.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import '../styles/style.css';
 
 function NoPageFound(){
     console.log('No page found');
     return(
-        <>
+        <div style = {{
+            
+                fontFamily: "newfont1",
+                fontSize: "100px",
+                textAlign: "center",
+                color: "rgb(0,0,0)",
+                position:"relative",
+        }}>
             No page found
-        </>
+        </div>
     )
 }
 
